e7729cef81041115dc122495d1060e8b506b842f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 17:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue){
-  return drivers.filter(d => d.revenue > revenue)
+  return drivers.filter(d => d.revenue > revenue);
 }
 
 function driverNamesWithRevenueOver(drivers, revenue){
-  return drivers.filter(d => d.revenue > revenue).map(d => d.name)
+  return driversWithRevenueOver(drivers, revenue).map(d => d.name);
 }
 
 function exactMatch(drivers, matcher){
-  return drivers.filter(d => Object.keys(matcher).every(k => matcher[k] === d[k]))
+  return drivers.filter(d => Object.keys(matcher).every(k => matcher[k] === d[k]));
 }
 
 function exactMatchToList(drivers, matcher){
-
+  return exactMatch(drivers, matcher).map(d => d.name);
 }
