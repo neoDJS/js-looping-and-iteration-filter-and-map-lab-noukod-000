bc763e918816ab7a98c97a4241ca9fb5dbf79d1d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 17:25:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function driverNamesWithRevenueOver(drivers, revenue){
   return drivers.filter(d => d.revenue > revenue).map(d => d.name)
 }
 
-function exactMatch(drivers, matcher){}
+function exactMatch(drivers, matcher){
+  return drivers.filter(d => Object(matcher).keys.every(k => matcher[k] === d[k]))
+}
